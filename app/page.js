b520460a7c4c29484f90aaa4642c88853e1fdfa4
zangetsu <dev.mehulsaini@gmail.com>
2024-01-17 02:45:29 +0530

    -

--- app/page.js
+++ app/page.js
@@ -1,16 +1,6 @@
-import OpenAI from "openai";
+import { redirect } from "next/navigation";
 
-export default function Home({children}) {
-  const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-
-  async function main() {
-    console.log("HERE");
-    const completion = await openai.chat.completions.create({
-      messages: [{ role: "user", content: "This is test message." }],
-      model: "gpt-3.5-turbo",
-    });
-    console.log(completion.choices[0]);
-  }
-
-  return ;
+export default function Home({ children }) {
+  redirect("/text-generator");
+  return;
 }
